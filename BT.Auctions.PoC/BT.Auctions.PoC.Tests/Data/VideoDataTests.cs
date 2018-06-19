a2ae292c623274a89b12e1d1a9b6b22112e211cc
nchong-barfoot <nchong@barfoot.co.nz>
2018-06-19 15:06:33 +1200

    Added unit test and index page to the wwwroot folder.

---
+++ BT.Auctions.PoC/BT.Auctions.PoC.Tests/Data/VideoDataTests.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using BT.Auctions.Poc.Data;
+using BT.Auctions.Poc.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BT.Auctions.PoC.Tests
+{
+    [TestClass]
+    public class VideoDataTests
+    {
+        [TestMethod]
+        public void GetVideosReturnsAListOfVideos()
+        {
+            VideoData videoData = new VideoData();
+            IEnumerable<Video> videos = videoData.GetVideos();
+
+            Assert.IsNotNull(videos);
+            Assert.AreEqual(videos.Count(), 6);
+        }
+    }
+}
